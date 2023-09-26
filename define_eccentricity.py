7c8fe9eecd5b755690bfe3cfd3bb6f1ce8bd02e8
suzannelexmond <suzannelexmond@gmail.com>
2023-09-26 17:31:16 +0200

    Residual Amp and Freq, time inependent of M. Also started def ecc.

--- define_eccentricity.py
+++ define_eccentricity.py
@@ -34,6 +34,50 @@ def SimInspiral(total_mass, mass_ratio, eccmin, freqmin, DeltaT = 1./2048., lalD
 
     return hp_TS, hc_TS
 
+@jit(target_backend='cuda')  # Explicitly set nopython=True
+def SimInspiral_t_over_M(total_mass, mass_ratio, eccmin, freqmin, DeltaT = 1./2048., lalDict = lal.CreateDict()):
+    """ Input: total_mass in M_sun, mass_ratio >= 1 (mass1 > mass2)
+        Output: 
+    
+    """
+
+    mass1 = total_mass/((1/mass_ratio) + 1)
+    mass2 = total_mass - mass1
+
+    timesTD, hp_TS, hc_TS = SimInspiral(mass1, mass2, eccmin, freqmin)
+    # time_before_merger = abs(timesTD + timesTD[-1])
+
+    t_over_M = timesTD / (lal.MTSUN_SI * total_mass )
+
+    hp_TS_over_M = hp_TS/total_mass
+    hc_TS_over_M = hc_TS/total_mass
+
+    norm_hp_TS_over_M = (hp_TS_over_M - hp_TS_over_M.min())/ (hp_TS_over_M.max() - hp_TS_over_M.min())
+    norm_hc_TS_over_M = (hc_TS_over_M - hc_TS_over_M.min())/ (hc_TS_over_M.max() - hc_TS_over_M.min())
+
+    return t_over_M, hp_TS, hc_TS
+
+def get_peaks_t_over_M(values, total_mass):
+
+    valuesmin = -values
+    pericenters_index, _per = find_peaks(values, height=0)
+    apocenters_index, _apo = find_peaks(valuesmin, height=-100)
+
+    pericenters = _per['peak_heights']
+    apocenters = -_apo['peak_heights']
+
+    return pericenters_index, pericenters, apocenters_index, apocenters
+
+
+def estimate_eccentricity(freq_per, freq_apo):
+    w_per, w_apo = freq_per*(2*np.pi), freq_apo*(2*np.pi)
+
+    w_ecc = np.zeros(len(freq_per))
+    for i in range(len(freq_per)):
+        # Freq epocenters has one more value than the freq pericenters. We start counting from apocenters[0] + i
+        w_ecc[i] = (np.sqrt(w_per[i]**2) - np.sqrt(w_apo[i + 1]**2)) / ((np.sqrt(w_per[i]**2) + np.sqrt(w_apo[i + 1]**2)))
+    
+    return w_ecc
 
 
 def plot_frequency(total_mass, mass_ratio, eccmin, freqmin, DeltaT = 1./2048., lalDict = lal.CreateDict()):
@@ -48,53 +92,39 @@ def plot_frequency(total_mass, mass_ratio, eccmin, freqmin, DeltaT = 1./2048., l
     fig1, axs = plt.subplots(3, figsize=(10, 10))
     plt.subplots_adjust(hspace=0.5)
 
-    axs[0].plot(-hp_TS.sample_times[::-1], hp_TS)
+    hp_times = -hp_TS.sample_times[::-1] / (lal.MTSUN_SI * total_mass )
+    amp_times = -amp.sample_times[::-1] / (lal.MTSUN_SI * total_mass )
+    freq_times = -freq.sample_times[::-1] / (lal.MTSUN_SI * total_mass )
+
+    axs[0].plot(hp_times, hp_TS)
     axs[0].set_ylabel('h$_{+}$')
     # plt.xlim(-7, 0)
-    axs[0].set_xlabel('Time (s)')
+    axs[0].set_xlabel('t [M]')
     axs[0].set_title('Waveform')
 
-
-
-
-    amp_times = -amp.sample_times[::-1]
-
-    ampmin = -amp
-    pericenters_index, _per = find_peaks(amp, height=0)
-    apocenters_index, _apo = find_peaks(ampmin, height=-100)
-
-    pericenters_amp = _per['peak_heights']
-    apocenters_amp = -_apo['peak_heights']
+    
+    amp_pericenters_index, amp_pericenters, amp_apocenters_index, amp_apocenters = get_peaks_t_over_M(amp, total_mass)
 
     axs[1].plot(amp_times, amp, color='orange')
-    axs[1].scatter(amp_times[pericenters_index], pericenters_amp, color='blue', s=5 , label='pericenters')
-    axs[1].scatter(amp_times[apocenters_index], apocenters_amp, color='magenta', s=5 , label='apocenters')
+    axs[1].scatter(amp_times[amp_pericenters_index], amp_pericenters, color='blue', s=5 , label='pericenters')
+    axs[1].scatter(amp_times[amp_apocenters_index], amp_apocenters, color='magenta', s=5 , label='apocenters')
     axs[1].set_ylabel('A$_+$')
-    axs[1].set_xlim(-7, 0)
+    axs[1].set_xlim(-20000, 0)
     # axs[1].set_ylim(0, 50)
-    axs[1].set_xlabel('t [s]')
+    axs[1].set_xlabel('t [M]')
     axs[1].legend(loc='upper left')
     axs[1].set_title('Amplitude Method')
-    # axs[0].invert_xaxis()
 
 
-    freq_times = -freq.sample_times[::-1]
-
-    freqmin = -freq
-    pericenters_index, _per = find_peaks(freq, height=0)
-    apocenters_index, _apo = find_peaks(freqmin, height=-100)
-
-    pericenters_freq = _per['peak_heights']
-    apocenters_freq = -_apo['peak_heights']
-
+    freq_pericenters_index, freq_pericenters, freq_apocenters_index, freq_apocenters = get_peaks_t_over_M(freq, total_mass)
 
     axs[2].plot(freq_times, freq, color='orange')
-    axs[2].scatter(freq_times[pericenters_index], pericenters_freq, color='blue', s=5 , label='pericenters')
-    axs[2].scatter(freq_times[apocenters_index], apocenters_freq, color='magenta', s=5 , label='apocenters')
+    axs[2].scatter(freq_times[freq_pericenters_index], freq_pericenters, color='blue', s=5 , label='pericenters')
+    axs[2].scatter(freq_times[freq_apocenters_index], freq_apocenters, color='magenta', s=5 , label='apocenters')
     axs[2].set_ylabel('f$_+$ = $\omega_+$/2$\pi$ [Hz]')
-    axs[2].set_xlim(-7, 0)
+    axs[2].set_xlim(-20000, 0)
     axs[2].set_ylim(0, 50)
-    axs[2].set_xlabel('t [s]')
+    axs[2].set_xlabel('t [M]')
     axs[2].legend(loc='upper left')
     axs[2].set_title('Frequency Method')
 
@@ -113,81 +143,92 @@ def plot_frequency(total_mass, mass_ratio, eccmin, freqmin, DeltaT = 1./2048., l
     amp_circ = amp_circ[len(amp_circ) - len(amp):]
     freq_circ = freq_circ[len(freq_circ) - len(freq):]
 
-    freq_times = -freq.sample_times[::-1]
-    amp_times = -amp.sample_times[::-1]
     
     # Residual frequency 
-    Res_f_t = np.zeros(len(freq_times))
-    Res_f = np.zeros(len(freq))
+    res_freq_times = np.zeros(len(freq_times))
+    res_freq = np.zeros(len(freq))
 
     for i in range(len(freq_times)):
-        Res_f_t[i] = freq_times[i]
-        Res_f[i] = freq[i] - freq_circ[i]
+        res_freq_times[i] = freq_times[i]
+        res_freq[i] = freq[i] - freq_circ[i]
 
-    Res_f_min = -Res_f
-    pericenters_index_f, _per = find_peaks(Res_f, height=0)
-    apocenters_index_f, _apo = find_peaks(Res_f_min, height=-100)
+    res_freq_pericenters_index, res_freq_pericenters, res_freq_apocenters_index, res_freq_apocenters = get_peaks_t_over_M(res_freq, total_mass)
 
-    pericenters_res_f = _per['peak_heights']
-    apocenters_res_f = -_apo['peak_heights']
 
     # Residual Amplitude
-    Res_A_t = np.zeros(len(amp_times))
-    Res_A = np.zeros(len(amp))
+    res_amp_times = np.zeros(len(amp_times))
+    res_amp = np.zeros(len(amp))
 
     for i in range(len(amp_times)):
-        Res_A_t[i] = amp_times[i]
-        Res_A[i] = amp[i] - amp_circ[i]
+        res_amp_times[i] = amp_times[i]
+        res_amp[i] = amp[i] - amp_circ[i]
 
-    Res_A_min = -Res_A
-    pericenters_index_amp, _per = find_peaks(Res_A, height=0)
-    apocenters_index_amp, _apo = find_peaks(Res_A_min, height=-100)
+    res_amp_pericenters_index, res_amp_pericenters, res_amp_apocenters_index, res_amp_apocenters = get_peaks_t_over_M(res_amp, total_mass)
 
-    pericenters_res_amp = _per['peak_heights']
-    apocenters_res_amp = -_apo['peak_heights']
-
-
+   
 
     fig2, axs = plt.subplots(3, figsize=(10, 10))
     plt.subplots_adjust(hspace=0.5)
 
-    axs[0].plot(-hp_TS.sample_times[::-1], hp_TS)
+    axs[0].plot(hp_times, hp_TS)
     axs[0].set_ylabel('h$_{+}$')
     # axs.xlim(-7, 0)
     axs[0].set_xlabel('Time (s)')
-    axs[0].set_title('Waveform')
+    axs[0].set_title('Waveform; total mass={}, mass ratio={}, eccmin={}, freqmin={}'.format(total_mass, mass_ratio, eccmin, freqmin))
 
     # axs[1].plot(amp_times, amp, label='A$_+$', color='orange' )
     # axs[1].plot(amp_times, amp_circ, label='A$_+^{circ}$', color='red')
-    axs[1].plot(Res_A_t, Res_A, label='Residual Amplitude')
-    axs[1].scatter(amp_times[pericenters_index_amp], pericenters_res_amp, color='blue', s=5 , label='pericenters')
-    axs[1].scatter(amp_times[apocenters_index_amp], apocenters_res_amp, color='magenta', s=5 , label='apocenters')
-    axs[1].set_ylabel('A$_+$')
-    # axs[1].set_xlim(-7, 0)
+    axs[1].plot(res_amp_times, res_amp, label='Residual Amplitude', color='orange')
+    axs[1].scatter(res_amp_times[res_amp_pericenters_index], res_amp_pericenters, color='blue', s=5 , label='pericenters')
+    axs[1].scatter(res_amp_times[res_amp_apocenters_index], res_amp_apocenters, color='magenta', s=5 , label='apocenters')
+    axs[1].set_ylabel('$\Delta$A$_+$')
+    # axs[1].set_xlim(-20000, 0)
     # axs[1].set_ylim(0, 50)
-    axs[1].set_xlabel('t [s]')
+    axs[1].set_xlabel('t [M]')
     axs[1].legend(loc='upper left')
     axs[1].set_title('Residual amplitude Method')
 
 
     # axs[2].plot(freq_times, freq, label='$\omega_+$', color='orange')
     # axs[2].plot(freq_times, freq_circ, label='$\omega_+^{circ}$', color='red')
-    axs[2].plot(Res_f_t, Res_f, label = 'Residual frequency')
-    axs[2].scatter(freq_times[pericenters_index_f], pericenters_res_f, color='blue', s=5 , label='pericenters')
-    axs[2].scatter(freq_times[apocenters_index_f], apocenters_res_f, color='magenta', s=5 , label='apocenters')
-    axs[2].set_ylabel('f$_+$ = $\omega_+$/2$\pi$ [Hz]')
-    # axs[2].set_xlim(-7, 0)
+    axs[2].plot(res_freq_times, res_freq, label = 'Residual frequency', color='orange')
+    axs[2].scatter(res_freq_times[res_freq_pericenters_index], res_freq_pericenters, color='blue', s=5 , label='pericenters')
+    axs[2].scatter(res_freq_times[res_freq_apocenters_index], res_freq_apocenters, color='magenta', s=5 , label='apocenters')
+    axs[2].set_ylabel('$\Delta$f$_+$ = $\Delta\omega_+$/2$\pi$ [Hz]')
+    # axs[2].set_xlim(-20000, 0)
     axs[2].set_ylim(-50, 50)
-    axs[2].set_xlabel('t [s]')
+    axs[2].set_xlabel('t [M]')
     axs[2].legend(loc='upper left')
     axs[2].set_title('Residual frequency Method')
 
-    plt.show()
-    figname = 'Residual_'
-    fig2.savefig('Images/' + figname)
+    # plt.show()
+    # figname = 'Residual_Amp_Freq_over_M_zoom'
+    # fig2.savefig('Images/' + figname)
 
-plot_frequency(50, 4, 0.3, 5)
+    ####################################################
+    # Eccentricity estimate
+    # print(len(res_amp_apocenters), len(res_amp_pericenters))
+    # print(res_amp_apocenters, res_amp_pericenters)
+    # print(len(res_freq_pericenters), len(res_freq_apocenters))
+    
+    res_ecc_w = estimate_eccentricity(res_freq_pericenters, res_freq_apocenters)
+
+    fig3, axs = plt.subplots(1, figsize=(10, 10))
+    plt.subplots_adjust(hspace=0.5)
+
+    axs.plot(res_freq_times[res_freq_pericenters_index], res_ecc_w)
+    axs.set_ylabel('ecc$_{w}$')
+    axs.set_xlabel('t [M]')
+
+    plt.show()
+
+
+
+plot_frequency(50, 4, 0.4, 5)
 # print('before')
 # SimInspiral(50, 4, 0.3, 5)
 # SimInspiral(50, 4, 1e-10, 5)
 # print('after')
+
+
+
